93e0d641cd9c44292d9dba55442a214497a0e1be
최수정 <tku5mklf@ewhain.net>
2020-11-23 19:44:11 +0900

    로그인 완성!

--- login.js
+++ login.js
@@ -1,12 +1,25 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+    authDomain: "lecture-story.firebaseapp.com",
+    databaseURL: "https://lecture-story.firebaseio.com",
+    projectId: "lecture-story",
+    storageBucket: "lecture-story.appspot.com",
+    messagingSenderId: "109177070261",
+    appId: "1:109177070261:web:8b6aa71008757f550254fc"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const auth=firebase.auth();
+
 //사용자 상태 인식 함수
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-      window.alert("로그인 됐습니다");
+      window.location.href="board.html";
     } else {
       // No user is signed in.
     }
-  });
+});
 
 //로그인 버튼 눌렀을 때 함수
 function login(){
@@ -19,5 +32,6 @@ function login(){
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
+        window.alert(errorMessage);
       });
 }
