dbadb8e0ffe7791d52d0cf749649ba545cb9d8f2
hoit1302 <68107000+hoit1302@users.noreply.github.com>
2020-11-23 21:56:48 +0900

    Revert "bar 모든 파일 적용"

--- javascript/timeline.js
+++
@@ -1,9 +0,0 @@
-function doDisplay(){
-    var con = document.getElementById("logout-menu");
-    if(con.style.display=='none'){
-        con.style.display='block';
-    }
-    else{
-        con.style.display='none';
-    }
-}
