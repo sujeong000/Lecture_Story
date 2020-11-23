3acbc127f4f037f04650cfd5540470929d5f8523
최수정 <tku5mklf@ewhain.net>
2020-11-23 17:35:36 +0900

    파이어베이스 시작

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+authDomain: "lecture-story.firebaseapp.com",
+databaseURL: "https://lecture-story.firebaseio.com",
+projectId: "lecture-story",
+storageBucket: "lecture-story.appspot.com",
+messagingSenderId: "109177070261",
+appId: "1:109177070261:web:8b6aa71008757f550254fc"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
