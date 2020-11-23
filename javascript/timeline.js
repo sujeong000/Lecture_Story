9a2bbd4fd62320aaded01b289dc146bb0df8a4c3
최수정 <tku5mklf@ewhain.net>
2020-11-23 22:00:59 +0900

    데이터베이스 시작 전

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+    authDomain: "lecture-story.firebaseapp.com",
+    databaseURL: "https://lecture-story.firebaseio.com",
+    projectId: "lecture-story",
+    storageBucket: "lecture-story.appspot.com",
+    messagingSenderId: "109177070261",
+    appId: "1:109177070261:web:8b6aa71008757f550254fc"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const auth=firebase.auth();
+
 function doDisplay(){
     var con = document.getElementById("logout-menu");
     if(con.style.display=='none'){
@@ -6,4 +19,13 @@ function doDisplay(){
     else{
         con.style.display='none';
     }
+}
+
+function logOut(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        window.location.href="login.html";
+      }).catch(function(error) {
+        // An error happened.
+      });
 }
