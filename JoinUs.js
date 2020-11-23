c9e1daf5b57960d9a7f6a7e8e564eba869a9d42a
lkh4317 <lkh4317@ewhain.net>
2020-11-23 18:54:42 +0900

    회원가입 firebase 연동중

---
+++ JoinUs.js
@@ -0,0 +1,24 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+    authDomain: "lecture-story.firebaseapp.com",
+    databaseURL: "https://lecture-story.firebaseio.com",
+    projectId: "lecture-story",
+    storageBucket: "lecture-story.appspot.com",
+    messagingSenderId: "109177070261",
+    appId: "1:109177070261:web:8b6aa71008757f550254fc"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const auth=firebase.auth();
+
+  function signUp(){
+  
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    
+    alert("Signed Up");
+   }
