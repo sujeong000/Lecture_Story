6851f8ef9c328bfebdcce19faadab0b10592af9b
Kang <mminjg@gmail.com>
2020-11-26 20:41:51 +0900

    select tag html, css

---
+++ public/javascript/write.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+    authDomain: "lecture-story.firebaseapp.com",
+    databaseURL: "https://lecture-story.firebaseio.com",
+    projectId: "lecture-story",
+    storageBucket: "lecture-story.appspot.com",
+    messagingSenderId: "109177070261",
+    appId: "1:109177070261:web:8b6aa71008757f550254fc"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  var db = firebase.firestore();
