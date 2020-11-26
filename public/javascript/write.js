8ece49cd87ffc5535e98a8732c56097116bb2c50
Kang <mminjg@gmail.com>
2020-11-26 22:11:00 +0900

    selectbox에 tag불러오기

--- public/javascript/write.js
+++ public/javascript/write.js
@@ -6,7 +6,28 @@ const firebaseConfig = {
     storageBucket: "lecture-story.appspot.com",
     messagingSenderId: "109177070261",
     appId: "1:109177070261:web:8b6aa71008757f550254fc"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
-  var db = firebase.firestore();
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+//tag 불러오기
+window.onload = function() {
+    var ref = db.collection("2020_1학기").doc("20479-이숙영").collection("tags");
+    var arr = [];
+    ref.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+        arr.push(doc.data().tag);
+        });
+        console.log(arr);
+        $("#select_tag").empty();
+
+        //tag 옵션에 추가
+        for(var i = 0; i<arr.length; i++) {
+            var option = $("<option>"+arr[i]+"</option>");
+            $('#select_tag').append(option);
+        }
+    });
+}
+
+//태그 추가할 때
