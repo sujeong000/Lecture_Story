d2f0a64fb50d6c41f5a9eedfb3b4ec4a44d440b9
hoit1302 <jueun000101@gmail.com>
2020-11-26 22:14:16 +0900

    board에서 timeline으로 넘어가는 코드 작성 중

--- public/javascript/board.js
+++ public/javascript/board.js
@@ -3,9 +3,41 @@
 //     console.log(getInput);
 //     localStorage.setItem("storageName",getInput);
 //  }
- 
+
 //과목명 검색
-function register(){
-    var search_key=document.getElementById("search").value;
-    localStorage.setItem("storageName",search_key);
- }
+function register() {
+    var search_key = document.getElementById("search").value;
+    localStorage.setItem("storageName", search_key);
+}
+var index;
+$(document).ready(function(){
+    $("section").click(function(){
+        // console.log($(".content").index($(this)));
+        index = $(".content").index($(this));
+    })
+})
+
+
+function move1() {
+    var info = docum/ent.getElementsByTagName("span")[index].innerHTML;
+    console.log(index + " " + info);
+    // location.href="../html/timeline.html";
+}
+
+function move(evt){
+    var text = evt.getElementsByTagName("span")[0].innerHTML;
+    console.log(text);
+    var textlist = text.split(' ');
+
+    console.log("11101");
+    localStorage.setItem("courseName", "11101");
+
+    console.log(textlist[0].split("(")[0]);
+    localStorage.setItem("courseName", textlist[0].split("(")[0]);
+
+    console.log(textlist[2]);
+    localStorage.setItem("prof", textlist[2]);
+
+    console.log("2020-2학기");
+    localStorage.setItem("semester" , "2020-2학기");
+}
