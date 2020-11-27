d26efe6e1fde707ab908daa49ff5982ad4c0359a
hoit1302 <jueun000101@gmail.com>
2020-11-27 23:56:13 +0900

    JoinUs 에러 해결 못함

--- public/javascript/JoinUs.js
+++ public/javascript/JoinUs.js
@@ -1,63 +1,49 @@
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCTexaR10Q0JKdVdUPrEhovRK5Mx_w-NO4",
-    authDomain: "lecture-story-2.firebaseapp.com",
-    databaseURL: "https://lecture-story-2.firebaseio.com",
-    projectId: "lecture-story-2",
-    storageBucket: "lecture-story-2.appspot.com",
-    messagingSenderId: "70634403692",
-    appId: "1:70634403692:web:5833a0adb975d77c186549"
+var firebaseConfig = {
+    apiKey: "AIzaSyBriqW5bb956O8Mi87iZJKtdNsD4uWGBp4",
+    authDomain: "lecture-story.firebaseapp.com",
+    databaseURL: "https://lecture-story.firebaseio.com",
+    projectId: "lecture-story",
+    storageBucket: "lecture-story.appspot.com",
+    messagingSenderId: "109177070261",
+    appId: "1:109177070261:web:8b6aa71008757f550254fc"
 };
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-
 const db = firebase.firestore();
+const form = document.querySelector('#JoinUsForm');
+// let userid;/
+// https://www.youtube.com/watch?v=qWy9ylc3f9U/
+form.addEventListener('submit', (e) => {
 
-function signUp() {
+    e.preventDefault();
 
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
     var password_check = document.getElementById("password_check");
 
-    var email_ch = email.value.split("@")[1];
-    if (email_ch != "ewhain.net") {
+    var email_ch = email.split("@")[1];
+    if (email_ch !== "ewhain.net") {
         document.test.Email.focus();
         alert("이화인 계정이어야 합니다.");
-    } else if (password.value === "") {
+    } else if (password === "") {
         alert("비밀번호를 입력하세요");
-    } else if (password.value.length < 6) {
+    } else if (password.length < 6) {
         alert("비밀번호는 6자 이상이어야 합니다");
-    } else if (password.value != password_check.value) {
+    } else if (password !== password_check.value) {
         document.test.p_check.focus();
         alert("비밀번호를 다시 확인해 주세요.");
     } else {
-        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-        promise.catch(e => console.log(e.message));
-
-        const form = document.querySelector('#JoinUsForm');
-
-        db.collection("UserInfo").add({
-            name: form.p_name.value,
-            year: form.p_year.value,
-            month: form.p_month.value,
-            day: form.p_day.value
+        auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            alert(cred.user.uid); /* 원하는 정보는 잘 찍히는데 밑에 코드(db에 저장하는 코드가 안먹음 먹었다안먹었다함...) */
+            return db.collection("UserInfo4").doc(cred.user.uid).set({
+                name: form.p_name.value
+            });
         });
-
-        // form.addEventListener("button",function() {
-        //     e.preventDefault();
-        //     var docdoc=db.collection("UserInfo").doc();
-        //     docdoc.add({
-        //         name:form.p_name.value,
-        //         year:form.p_year.value,
-        //         month:form.p_month.value,
-        //         day:form.p_day.value
-        //     });
-        //     console.log(form.p_year.value);
-        // });
-
     }
-}
+});
+
 //회원가입 화면에서 board.html로 간다
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
