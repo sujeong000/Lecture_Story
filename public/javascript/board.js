4f0731affdd10092efde0cf2fb6987a0e9fa7c88
hoit1302 <jueun000101@gmail.com>
2020-11-28 13:06:26 +0900

    유저정보에 맞는 즐겨찾기를 띄우기3

--- public/javascript/board.js
+++ public/javascript/board.js
@@ -71,10 +71,12 @@ function move(evt) {
 //         }
 //     });
 
+
 function loadPage() {
+    
     let html = '';
     db.collection("Users")
-        .doc(firebase.firestore().currentUser.uid)
+        .doc(auth.currentUser.uid)
         .collection("즐겨찾기")
         .get()
         .then(function (querySnapshot) {
@@ -100,5 +102,8 @@ function loadPage() {
             container.innerHTML = html;
         });
 }
-loadPage();
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user)
+        loadPage();
+});
