35ef73d7f793d0d4854a2b80b83daab201046539
lkh4317 <lkh4317@ewhain.net>
2020-12-03 16:08:06 +0900

    검색어 대소문자 구분 없앰, 주석 정리

--- public/javascript/select_lecture.js
+++ public/javascript/select_lecture.js
@@ -28,14 +28,14 @@ function register(){
 }
 
 
-//검색어: [  ] 표시하는 부분
+//검색어:--- 표시하는 부분
 var storageKey=localStorage.getItem("storageName");
 document.getElementById("search_key").innerHTML=storageKey;
 
 //board에서 학기 전달 받음
 var semester = localStorage.getItem("semester");
 
-//전달받은 학기로 option의 select를 바꿔둔다 //html에서 selected지움
+//전달받은 학기로 option의 select를 바꿔둔다
 if (semester === "2020_2학기") {
   document.getElementsByTagName('option')[0].selected = "selected";
 } else if (semester === "2020_1학기") {
@@ -46,8 +46,15 @@ if (semester === "2020_2학기") {
   document.getElementsByTagName('option')[3].selected = "selected";
 } 
 
+//데이터베이스
+var db = firebase.firestore();
+var auth=firebase.auth();
 
-// 학기 select 박스에서 학기를 변경할 경우 작동하는 함수
+//처음에 과목리스트 불러오기
+window.onload=show_lec();
+
+
+// 학기 select 박스에서 학기를 변경할 경우, 자동으로 과목 리스트 변경
 function change_tag(){
   // html에서 학기 이름 따오기
   var tag_choice = document.querySelector(".semester");
@@ -57,30 +64,24 @@ function change_tag(){
   // 학기 이름 저장하고 학기 이름 변경
   localStorage.setItem("semester", tag_selected);
   semester = localStorage.getItem("semester");
-  //새로 가져오기 전에 radio파트 삭제
-  $('ul').empty();
-  window.onload=show_lec();
+
+  $('ul').empty(); //과목리스트 새로 가져오기 전, 이전 것은 지우기
+  window.onload=show_lec(); //새로 불러오기
 }
 
 
-//데이터베이스
-var db = firebase.firestore();
-var auth=firebase.auth();
-
-window.onload=show_lec();
-
-
-//"이산"만 검색해도 "이산수학"이 검색되도록 했음
 //검색어 읽어오기
+//"이산"만 검색해도 "이산수학"이 검색되도록 했음
 function show_lec() {
+  //과목명에 영어가 포함되는 경우 전부 대문자이므로 toUpperCase()사용
+  var upper_storageKey=storageKey.toUpperCase();
   var ref = db.collection(semester);
   ref.get().then(function (querySnapshot) {
-    //let html = '';
     var arr = new Array;
     querySnapshot.forEach((doc) => {
-      if(doc.data().교과목명.includes(storageKey)){
+      if(doc.data().교과목명.includes(upper_storageKey)){
         arr.push(doc.id);
-      createLine(doc);
+      createLine(doc); //과목리스트 생성
       }
     });
   }).catch(function (error) {
@@ -89,42 +90,35 @@ function show_lec() {
  
 }
 
-//radio에서 checkbox로 변경
 //checkbox 파트 만들기
 function createLine(doc){
   var num=doc.data().학수번호;
   var name=doc.data().교과목명;
   var pf=doc.data().교수명;
   var cl=doc.data().분반;
-  
-  // //읽어온 검색어를 사용자에게 보여줘야 함. 어떤 과목을 추가할 건지
-  // var str=name+" ("+num+" - "+cl+") - "+pf+" 교수님";
-  // line="<input type='checkbox' id='"+str+"'>"+
-  // "<label for='"+str+"'><span style='padding:15px; font-weight:bold; font-size:20px; line-height:2.5em;'>"+str+"<br></span></label>";
-  // $("ul").append(line); //jquery문법, html에 링크 추가함
 
-  //읽어온 검색어를 사용자에게 보여줘야 함. 어떤 과목을 추가할 건지
+  //html에 내용 동적으로 추가
   //라벨을 사용해서 체크박스 옆의 텍스트만 선택해도 과목 선택이 되도록 함
   var str=name+" ("+num+" - "+cl+") - "+pf+" 교수님<br>";
   line="<li><input type='checkbox' id='"+str+"'>"+
   "<label for='"+str+"'><span '>"+str+"</span></label></li>";
-  $("ul").append(line); //jquery문법, html에 링크에 위의 요소 추가함 추가함
+  $("ul").append(line); //jquery문법, html head에 링크 추가
 
 };
 
 
-//즐겨찾기 추가한거 정보 보내기
+//즐겨찾기 선택한 과목의 정보 데이터베이스에 추가
 function sendInfo(){
   var rei=document.getElementsByTagName("input");
   var s=document.getElementsByTagName("span");
   var check_decision=false;
   //s -> span 태그 list => 0번은 검색어 나타내는 부분
-  //그 뒤로 radio 옆의 과목명,학수번호,분반,교수님 나오는 문자열
+  //그 뒤로 체크박스 옆에 과목명,학수번호,분반,교수님 나오는 문자열
   for(i=0;i<rei.length;i++){
     if(rei[i].checked===true){
       //rei -> 0번에는 검색창, 그뒤로는 체크박스버튼
-      check_decision=true;
-      splitStr(s[i].innerHTML); //쪼개기
+      check_decision=true;//버튼이 체크 되어있으면
+      splitStr(s[i].innerHTML); //문자열 쪼개기
     }
   }
   //아무 선택도 하지 않으면 팝업창 보내기
@@ -143,7 +137,7 @@ function splitStr(str) {
   var lec_class = strArr[3].split(")")[0];//분반
   var lec_pf = strArr[5];//교수님
 
-  //즐겨찾기 추가
+  //즐겨찾기 데이터베이스에 추가
   var docdoc = db.collection("Users").doc(firebase.auth().currentUser.uid).collection("즐겨찾기");
   docdoc.doc(lec_name + "-" + lec_pf).set({
     교과목명: lec_name,
